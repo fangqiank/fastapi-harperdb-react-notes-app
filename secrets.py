a29c7ffb1228f31a5af4a2e37f690e0e8c62f01d
fangq <fangqiank@msn.com>
2022-06-03 13:46:19 +0800

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,7 +1,7 @@
-# HARPERDB_URL = 'https://myharper-live.harperdbcloud.com'
-# HARPERDB_USERNAME = 'huamu'
-# HARPERDB_PASSWORD = 'g73gle73'
+# HARPERDB_URL = '*'
+# HARPERDB_USERNAME = '*'
+# HARPERDB_PASSWORD = '*'
 
-HARPERDB_URL = 'http://192.168.5.1:9925'
-HARPERDB_USERNAME = 'admin'
-HARPERDB_PASSWORD = 'g73gle73'
+HARPERDB_URL = '*'
+HARPERDB_USERNAME = '*'
+HARPERDB_PASSWORD = '*'
